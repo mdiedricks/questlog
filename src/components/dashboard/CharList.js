9ffeb0ef8e6702c0479a68969f0dc1ed08d76fa7
mdiedricks <michael.diedricks@gmail.com>
2020-02-12 21:00:40 +1100

    correcting api key upload

--- src/components/dashboard/CharList.js
+++ src/components/dashboard/CharList.js
@@ -31,34 +31,34 @@ const heroes = [
 
 export default function CharList() {
   return (
-    <div className="col l4 m8 s12">
+    <div className="col l4 m8 s12 dash-container">
       <h5>Heroes</h5>
       <ul list-style-type="none">
         {heroes.map(e => (
           <li key={e.name}>
-            <div className="card horizontal small light-green lighten-3 z-depth-2">
+            <div className="card horizontal small orange lighten-5 z-depth-2">
               <div className="card-image">
                 <img
                   src="https://i.pinimg.com/originals/57/ca/6d/57ca6d159674aee8d0e01166850dd8e4.jpg"
-                  alt="hero"
+                  alt={e.name}
                 />
               </div>
               <div className="card-stacked">
                 <div className="card-content small left-align">
-                  <h5>{e.name}</h5>
+                  <span className="char-title">{e.name}</span>
                   <p>Race: {e.race}</p>
                   <p>Class: {e.class}</p>
                   <p>HP: {e.hp}</p>
                   <p>AC: {e.ac}</p>
                   <p>Level: {e.level}</p>
                 </div>
-                <div className="card-action">
-                  <button className="btn-flat">
+                <div className="right-align">
+                  <div className="btn-flat">
                     <i className="tiny material-icons">edit</i>
-                  </button>
-                  <button className="btn-flat">
+                  </div >
+                  <div className="btn-flat">
                     <i className="tiny material-icons">close</i>
-                  </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -69,6 +69,6 @@ export default function CharList() {
       <button className="btn-flat">
         <i className="tiny material-icons">person_add</i>
       </button>
-    </div>
+    </div >
   );
 }
--- src/components/dashboard/DashHome.js
+++ src/components/dashboard/DashHome.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 // import firebase from 'firebase';
 
 class DashHome extends Component {
+  // const db = db.collection('games');
+
   render() {
     return (<div>
       <h2>Welcome to Questlog</h2>
       <p>Login to create a new game for you and your group.</p>
+      {}
     </div>
     );
   }
